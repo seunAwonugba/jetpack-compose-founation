4b6c31ec7bf0248e7eb93fd2dd78bb0275eaf461
seunawonugba <seun.awonugba@decagon.dev>
2021-12-28 02:10:11 +0100

    learn staste with input field

--- app/src/main/java/com/example/thinkingincompose/MainActivity.kt
+++ app/src/main/java/com/example/thinkingincompose/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.thinkingincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.thinkingincompose.ui.theme.ThinkingInComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +22,16 @@ class MainActivity : ComponentActivity() {
                     val state = remember {
                         mutableStateOf(0)
                     }
-                    ClickCounter(clicks = state.value, onClick = {state.value++} )
+
+                    Column(modifier = Modifier.padding(8.dp)) {
+                        Spacer(modifier =Modifier.height(8.dp))
+                        ClickCounter(clicks = state.value, onClick = {state.value++} )
+                        Spacer(modifier =Modifier.height(8.dp))
+
+                        InputField()
+
+                    }
+
 
                 }
             }
@@ -38,4 +45,20 @@ fun ClickCounter(clicks: Int, onClick: () -> Unit) {
     Button(onClick = onClick) {
         Text("I've been clicked $clicks times")
     }
+}
+
+//state practice
+
+@Composable
+fun InputField(){
+    var userInput by remember {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        value = userInput,
+        onValueChange = {userInput = it},
+        label = { Text(text = "Firstname")},
+        placeholder = { Text(text = "Firstname")},
+        modifier = Modifier.fillMaxWidth()
+    )
 }
